554c9229a039c279ffbc96504f49e58ea930e1b7
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-02 05:51:28 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
-        System.out.println(Soluutions.reNumbersAscending(s));
-
-
-
-
-
+        
 
 
     }
