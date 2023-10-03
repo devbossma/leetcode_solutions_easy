def492ffd05780ab957b90cbd9e491ed3e45cd12
devbossma <ysabere201@gmail.com>
2023-10-03 02:07:30 +0100

    42. Trapping Rain Water

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
 import easy.Soluutions;
+import hard.Solutions;
 
 import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
-        System.out.println(Soluutions.reNumbersAscending(s));
-
-
-
-
-
-
 
     }
-
 
 }
---
+++ src/hard/Solutions.java
@@ -0,0 +1,51 @@
+package hard;
+
+import java.util.*;
+
+public class Solutions {
+    public static int trap(int[] height) {
+        int trapped = 0;
+        int currentElevation = 0;
+        int maxElevation = 0;
+        int secondMaxElevation = 0;
+        int indexOfMaxElevation = 0;
+        int indexOfSecondElevation = 0;
+
+        List lHeight = new ArrayList<>();
+
+
+        while (currentElevation < height.length){
+            if (height[currentElevation] >= maxElevation) {
+                maxElevation = height[currentElevation];
+                indexOfMaxElevation = currentElevation;
+            }
+            lHeight.add(height[currentElevation]);
+            currentElevation++;
+        }
+        for (int i = 0; i < height.length; i++) {
+            if(height[i] <= maxElevation && height[i] > secondMaxElevation){
+                secondMaxElevation = height[i];
+                indexOfSecondElevation = i;
+            }
+        }
+        int curentMaxElevation=0;
+        for (int i = 0; i < height.length-1; i++) {
+            if((i < indexOfSecondElevation && i < indexOfMaxElevation)){
+                if (height[i] > curentMaxElevation) curentMaxElevation = height[i];
+                trapped += curentMaxElevation - height[i];
+            }
+            if (i > indexOfSecondElevation && i < indexOfMaxElevation) {
+                trapped += secondMaxElevation - height[i];
+            }
+            if (i > indexOfMaxElevation){
+                curentMaxElevation = height[i];
+                for (int j = i; j < height.length; j++) {
+                    if (height[j] > curentMaxElevation) curentMaxElevation = height[j];
+                }
+                trapped += curentMaxElevation - height[i];
+            }
+        }
+        return trapped;
+    }
+}
+
