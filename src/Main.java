d8e5d43a51c991bd59d9b22c268a31e45f752526
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-03 02:09:40 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import easy.Soluutions;
 import hard.Solutions;
+import medium.Solutions;
 
 import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
+        
 
     }
 
