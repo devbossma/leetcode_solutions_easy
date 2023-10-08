a7d63badbfac36eea9eda24e3ca233267f590f47
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-08 23:23:32 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,5 @@ import static hard.Solutions.largestRectangleArea;
 
 public class Main {
     public static void main(String[] args) {
-        int[] heights = {2,1,5,6,2,3};
-
-        int max = largestRectangleArea(heights);
-        System.out.println("**** max: ["+max+"] ****");
-    }
+       
 }
