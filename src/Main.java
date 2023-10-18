3e70f7eb3f583508df6d0490927e6034b80eb847
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-18 23:32:39 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,4 @@ import static hard.Solutions.largestRectangleArea;
 
 public class Main {
     public static void main(String[] args) {
-        
-        /*
-        // Largest Rectangle in Histogram
-        int[] heights = {2,1,5,6,0,3};
-        int[] heights = {0,1,0,6,0,10};
-
-        int max = largestRectangleArea(heights);
-        System.out.println("**** max: ["+max+"] ****");
-        */
-       
 }
